bc15c424ab3ff52ad3512033535866a26c6d4b3b
Donghoon Lee <movehoon@gmail.com>
2024-01-17 10:52:15 +0900

    first commit

---
+++ setup.py
@@ -0,0 +1,26 @@
+from setuptools import setup
+
+package_name = 'line_sen'
+
+setup(
+    name=package_name,
+    version='0.0.0',
+    packages=[package_name],
+    data_files=[
+        ('share/ament_index/resource_index/packages',
+            ['resource/' + package_name]),
+        ('share/' + package_name, ['package.xml']),
+    ],
+    install_requires=['setuptools'],
+    zip_safe=True,
+    maintainer='robot',
+    maintainer_email='movehoon@gmail.com',
+    description='TODO: Package description',
+    license='TODO: License declaration',
+    tests_require=['pytest'],
+    entry_points={
+        'console_scripts': [
+            'line_sen_node = line_sen.line_sen_node:main'
+        ],
+    },
+)
